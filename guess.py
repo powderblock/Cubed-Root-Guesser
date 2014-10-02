15666e475287be67f3e81d0c9ec28b242f247b2f
Blade Nelson <powderblock@gmail.com>
2014-10-01 21:15:32 -0400

    Do ** for cube
    do **  for cube of number instead of x * x * x.

--- guess.py
+++ guess.py
@@ -2,8 +2,8 @@ import random
 
 random.seed()
 
-number = random.randrange(9, 99)
-numberCubed = number * number * number
+number = random.randrange(0, 99)
+numberCubed = number ** 3
 
 print(numberCubed)
 wait = raw_input()
